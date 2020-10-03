6f9981843d8f1059fcd4a909ba29d4be145f45f8
Your Name <belajarayok866@gmail.com>
2020-10-03 14:13:04 +0700

    first commit

---
+++ no2.js
@@ -0,0 +1,7 @@
+var nilaiSantri = [ 90, 60, 65, 97, 99, 60, 53, 91, 89 ];
+var total=0;
+// var baru= nilaiSantri.length;
+for(i=0; i<nilaiSantri.length; i++){
+    total+=nilaiSantri[i]
+}
+document.write(total/nilaiSantri.length);
